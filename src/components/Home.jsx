53c629a24bb71d8282706403b8045a38520f7e7e
1Canis-Lupus1 <shubhamchoudhary081997@gmail.com>
2021-01-04 11:38:27 +0530

    Carousel Added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Carousel } from 'antd';
 
 function Home() {
+    const contentStyle = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
+
     return (
         <div>
             <h1>Home for SS Finance</h1>
+            <h3>Carousel here</h3>
+            <Carousel autoplay>
+    <div>
+      <h3 style={contentStyle}>1</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>2</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>3</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>4</h3>
+    </div>
+  </Carousel>
         </div>
     )
 }

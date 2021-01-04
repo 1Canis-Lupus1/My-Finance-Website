2ad3386be30fa0e3f3a955456c9739ab3e601751
1Canis-Lupus1 <shubhamchoudhary081997@gmail.com>
2021-01-04 14:01:23 +0530

    Carousel pics added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,35 +1,39 @@
-import React from 'react'
-import { Carousel } from 'antd';
+import React from "react";
+import { Carousel } from "antd";
+import Img1 from "../assets/Finance1.jpeg";
+import Img2 from "../assets/Finance2.png";
+import Img3 from "../assets/Finance3.jpeg";
+import Img4 from "../assets/Finance4.jpeg"
 
 function Home() {
-    const contentStyle = {
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
-};
+  const contentStyle = {
+    height: "160px",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "#364d79",
+  };
 
-    return (
+  return (
+    <div>
+      <h1>Home for SS Finance</h1>
+      <h3>Carousel here</h3>
+      <Carousel autoplay>
         <div>
-            <h1>Home for SS Finance</h1>
-            <h3>Carousel here</h3>
-            <Carousel autoplay>
-    <div>
-      <h3 style={contentStyle}>1</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>2</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>3</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>4</h3>
-    </div>
-  </Carousel>
+          <img src={Img1} style={contentStyle,{width:"100%"}} />
         </div>
-    )
+        <div>
+          <img src={Img2} style={contentStyle,{width:"100%"}} />
+        </div>
+        <div>
+          <img src={Img3} style={contentStyle,{width:"100%"}} />
+        </div>
+        <div>
+          <img src={Img4} style={contentStyle,{width:"100%"}} />
+        </div>
+      </Carousel>
+    </div>
+  );
 }
 
-export default Home
+export default Home;

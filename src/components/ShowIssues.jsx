5074b7066d6202518642825d53ec5d3874799d3a
1Canis-Lupus1 <shubhamchoudhary081997@gmail.com>
2021-01-08 12:29:32 +0530

    Issue Psedu commit

--- src/components/ShowIssues.jsx
+++ src/components/ShowIssues.jsx
@@ -1,13 +1,34 @@
 import React, { Component } from "react";
-import { Card, Button } from "antd";
+import { Card, Button, Alert, Badge, Modal } from "antd";
+import moment from "moment";
 
 export class ShowIssues extends Component {
   constructor(props) {
     super(props);
     this.state = {
       userList: [],
+      isModalVisible: false,
     };
   }
+
+  showModal = () => {
+    this.setState({
+      isModalVisible: true,
+    });
+  };
+
+  handleOk = () => {
+    this.setState({
+      isModalVisible: false,
+    });
+  };
+
+  handleCancel = () => {
+    this.setState({
+      isModalVisible: false,
+    });
+  };
+
   componentDidMount() {
     var userArr = JSON.parse(localStorage.getItem("users"));
     console.log("Check 1:", userArr);
@@ -31,15 +52,29 @@ export class ShowIssues extends Component {
     return (
       <div>
         {this.state.userList.length === 0 ? (
-          <Card style={{ width: 300 }}>
-            <h1>No Issues To Display</h1>
-          </Card>
+          <div class="card text-center">
+            <div class="card-header card bg-success">
+              <h2 style={{ color: "white", paddingBottom: "0px" }}>
+                <b>No Pending Isues</b>
+              </h2>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">
+                Navigate To <b>Contact Us</b> for adding a new issue
+              </h5>
+            </div>
+          </div>
         ) : (
           this.state.userList.map((e, i) => {
             console.log("Each is:", e);
             return (
               <>
-                <h3>ISSUE NUMBER #{i + 1}</h3>
+                <h5>
+                  <b>
+                    ISSUE #{i + 1} (Added On:{" "}
+                    {moment(Date.now()).format("dddd, MMMM Do YYYY")})
+                  </b>
+                </h5>
                 <table class="table table-dark">
                   <thead>
                     <tr>
@@ -49,7 +84,7 @@ export class ShowIssues extends Component {
                       <th scope="col">Email</th>
                       <th scope="col">Issue</th>
                       <th scope="col">Status</th>
-                      <th scope="col">Admin Panel</th>
+                      {/* <th scope="col">Admin Panel</th> */}
                     </tr>
                   </thead>
                   <tbody>
@@ -60,15 +95,11 @@ export class ShowIssues extends Component {
                       <td>{e.email}</td>
                       <td>{e.issue}</td>
                       <td>
-                        <Button disabled>
-                          <b style={{ color: "black" }}>Issue Pending</b>
+                        <Button style={{ backgroundColor: "green" }} disabled>
+                          <b style={{ color: "white" }}>Pending</b>
                         </Button>
                       </td>
-                      <td>
-                        <Button>
-                          <b style={{ color: "black" }}>Issue Resolved</b>
-                        </Button>
-                      </td>
+                      <td></td>
                     </tr>
                   </tbody>
                 </table>
